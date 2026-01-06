c5e39df49e79af31753d7a625b874933ce800189
Ashlee Esprecion <adesprecion@student.apc.edu.ph>
2026-01-06 11:18:16 +0800

    Fix Images

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,5 +1,4 @@
-
-  const app = Vue.createApp({
+const app = Vue.createApp({
    data() {
     return {
      message: "Hello World WEBPROG IT242!"
@@ -10,22 +9,3 @@
   app.mount('#app')
  
  
- 
-
-const app2 = Vue.createApp({
-   data() {
-    return {
-      manyFoods: [
-        'https://www.w3schools.com/vue/img_burrito.svg',
-        'https://www.w3schools.com/vue/img_salad.svg',
-        'https://www.w3schools.com/vue/img_cake.svg',
-        'https://www.w3schools.com/vue/img_soup.svg',
-        'https://www.w3schools.com/vue/img_fish.svg',
-        'https://www.w3schools.com/vue/img_pizza.svg',
-        'https://www.w3schools.com/vue/img_rice.svg'
-      ]
-    }
-   }
-  })
-  
-  app2.mount('#app2')
