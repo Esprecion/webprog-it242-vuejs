f64f64b20920dafce7b980839b31ba5a5c477189
Ashlee Esprecion <adesprecion@student.apc.edu.ph>
2026-01-16 11:19:36 +0800

    New trial no4

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,5 +1,4 @@
 <template>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -164,7 +163,6 @@
         }).mount('#app')
     </script>
 </body>
-</html>
 
 </body>
 </template>
