3e8e91aa29cd389bcd425edb52e232c0d78d16a4
Ashlee Esprecion <adesprecion@student.apc.edu.ph>
2026-01-27 10:51:13 +0800

    added comment

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -5,11 +5,16 @@ import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import FoodItem3 from './components/FoodItem3.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('food-item3', FoodItem3)
 app.component('personal-profile', PersonalProfile)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
+
 
 app.mount('#app')
